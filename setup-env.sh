681f9e2f26efd38ff03a8890d3ad63944fa38b86
TechDevangelist <97691709+TechDevangelist@users.noreply.github.com>
2022-03-21 13:56:28 -0400

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -3,4 +3,6 @@ export PREFIX=/usr
 export PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/:/usr/local/lib/pkgconfig/
 export CC="arm-linux-gnueabihf-gcc"
 export CXX="arm-linux-gnueabihf-g++"
+export EDITOR=nano
+export VISUAL="$EDITOR"
 # apt update >/dev/null 2>&1 &
